c244b5a152e2146e25d4efea891e0a6ee3b75a05
ozgu kaptan <ozgukp@hotmail.com>
2021-03-14 00:08:35 +0300

    14-03-2021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
-import {HttpClient} from '@angular/common/http';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { ProductService } from 'src/app/services/product.service';
-
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
- 
-  products : Product[] = [];
+  products: Product[] = [];
   dataLoaded = false;
   
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService, 
+    private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.getProducts();
+      this.activatedRoute.params.subscribe(params=>{
+        if(params["categoryId"]){
+          this.getProductsByCategory(params["categoryId"])
+        }else{
+          this.getProducts()
+        }
+      })
   }
 
-  // gelen datayı ProductResponseModel e map eder!!
-  // Async çalısır
-  getProducts(){
+  getProducts() {
     this.productService.getProducts().subscribe(response=>{
       this.products = response.data
       this.dataLoaded = true;
-    })
+    })   
+  }
+
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })   
   }
 }
